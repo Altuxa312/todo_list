630a2ccb6b2b41feed6d558b58234a689ecb686c
Altuxa312 <urmatbekovanarbek777@gmail.com>
2022-06-02 19:05:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+    // const [text, setText] = useState('')
+  // const [todo, setTodo] = useState([])
+  // function addTodo() {
+  //   setTodo([...todo, text])
+  //   setText('')
+  // }
+
+  const [play, setPlay] = useState('')
+  const [tery, setTery] = useState([])
+function addTodo(){
+  setTery([...tery, play])
+  setPlay('')
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className = "App">
+          {/* <div>
+        <input type="text"
+          value={text}
+          placeholder='Нипишите'
+          onChange={a => setText(a.target.value)} />
+        <button onClick={addTodo}>Enter</button>
+        <ol>
+          {todo.map(item => (
+            <li>
+              {item}
+            </li>
+          ))}
+        </ol>
+      </div> */}
+
+<div>
+<input type="text"
+value={play}
+placeholder='пишите'
+onChange={a => setPlay(a.target.value)} />
+<button onClick={addTodo}>запуск</button>
+  <ol>
+{tery.map(item => (
+  <li>
+  {item}
+  </li>
+))}
+  </ol>
+
+</div>
     </div>
   );
 }
